f817f6aedb1513dc3c867a5c083a2ea8366b399e
BOOK-BVBUIR6SCP\amk02 <amk0223@naver.com>
2025-11-20 14:26:13 +0900

    bikeway백서버입니다.

---
+++ src/main/java/com/hulahoop/bikewayback/model/dao/ReservationMapper.java
@@ -0,0 +1,12 @@
+package com.hulahoop.bikewayback.model.dao;
+
+import com.hulahoop.bikewayback.model.dto.ReservationDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ReservationMapper {
+    List<ReservationDTO> findByMemberCode(@Param("memberCode") int memberCode);
+}
